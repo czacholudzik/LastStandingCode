259300c558a47397afae6e5351516dbfd4082b02
czacholudzik <lekrasujewski@gmail.com>
2021-04-05 20:46:15 +0200

    01

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject PanelGraj;
+    public GameObject PanelUstawienia;
+    public GameObject PanelWyjdz;
+    public AudioClip Guzik;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Btn_Graj()
+    {
+        if (PanelGraj != null)
+        {
+            PanelGraj.SetActive(true);
+        }
+    }
+
+    public void Btn_TrybTutorial()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Btn_Ustawienia()
+    {
+        if(PanelUstawienia != null)
+        {
+            PanelUstawienia.SetActive(true);
+        }
+    }
+
+    public void BtnPowrot()
+    {
+        if(PanelUstawienia != null)
+        {
+            PanelUstawienia.SetActive(false);
+        }
+    }
+
+    public void Btn_Wyjdz()
+    {
+        if(PanelWyjdz != null)
+        {
+            PanelWyjdz.SetActive(true);
+        }
+    }
+
+    public void Btn_WWyjdz()
+    {
+        Application.Quit();
+    }
+
+    public void Btn_AAnuluj()
+    {
+        if(PanelWyjdz != null)
+        {
+            PanelWyjdz.SetActive(false);
+            Guzik.LoadAudioData();
+        }
+    }
+}
